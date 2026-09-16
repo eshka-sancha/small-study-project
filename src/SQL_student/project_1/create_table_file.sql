6b435f314bde5ea1ccb340362d9bf81bb331b351
Alexandra <154466556+eshka-sancha@users.noreply.github.com>
2026-09-16 12:38:40 +0300

    Update create_table_file.sql

--- src/SQL_student/project_1/create_table_file.sql
+++ src/SQL_student/project_1/create_table_file.sql
@@ -1,1 +1,14 @@
+-- создание таблицы клиентов --
+CREATE TABLE client (
+	id_client SERIAL PRIMARY KEY,
+	name VARCHAR(20),
+	birthdate DATE,
+	addres VARCHAR(100)
+);
 
+-- создание таблицы покупок --
+CREATE TABLE item (
+	id_item SERIAL PRIMARY KEY,
+	name_item VARCHAR(20),
+	description VARCHAR(200)
+);
