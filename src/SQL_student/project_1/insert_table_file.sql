e02f1bfb60c59d7e664497b10c02e79d55edd4f5
Alexandra <154466556+eshka-sancha@users.noreply.github.com>
2026-09-16 12:40:24 +0300

    Create insert_table_file.sql

---
+++ src/SQL_student/project_1/insert_table_file.sql
@@ -0,0 +1,23 @@
+INSERT INTO client(name, birthdate, addres)
+VALUES ('Anna', '2000-11-09', 'Moscow'), 
+		('Boris', '1998-01-30', 'Saint Petersburg'), 
+		('Anton B', '1998-02-23', 'Moscow'), 
+		('Nadezhda', '2002-04-30', 'Samara'), 
+		('Anton A', '2005-01-06', 'Ulyanovsk'), 
+		('Anton S', '2006-12-14', 'Saint Petersburg'), 
+		('Ivan', '1970-07-12', 'Noyabrsk'), 
+		('Alexandra', '2005-04-30', 'Novosibirsk'), 
+		('Anton G', '1998-02-23', 'Moscow'),
+		('Angelina', '1999-11-11', 'Saint Petersburg');
+		
+INSERT INTO item(name_item, description)
+VALUES ('hand cream', 'moisturizing hand cream with aloe and collagen'), 
+		('3-in-1 shampoo', 'moisturizing, growth-promoting shampoo with mint scent. suitable for body and face'), 
+		('3-in-1 shampoo', 'moisturizing, growth-promoting shampoo with mint scent. suitable for body and face'), 
+		('hand cream', 'moisturizing hand cream with aloe and collagen'), 
+		('laundry soap', 'ordinary soap. pH level 8.5 - 12'), 
+		('laundry detergent', 'powder for white clothes, whitening'), 
+		('hair spray', 'strong-hold hair spray for thick hair'), 
+		('face toner', 'cleansing product with low pH. for all skin types'), 
+		('laundry soap', 'ordinary soap. pH level 8.5 - 12'),
+		('nail polishes', 'set of gel polishes, 3 colors');
