527df1d7e031348b7a251d096ca7c082bee2e21d
Alexandra <154466556+eshka-sancha@users.noreply.github.com>
2026-09-16 12:39:39 +0300

    Update create_table_file.sql

--- src/SQL_student/project_1/create_table_file.sql
+++ src/SQL_student/project_1/create_table_file.sql
@@ -6,9 +6,19 @@ CREATE TABLE client (
 	addres VARCHAR(100)
 );
 
--- создание таблицы покупок --
+-- создание таблицы товаров --
 CREATE TABLE item (
 	id_item SERIAL PRIMARY KEY,
 	name_item VARCHAR(20),
 	description VARCHAR(200)
+);
+
+-- создание таблицы покупок товаров клиентами --
+CREATE TABLE buy (
+	id_client INT,
+	id_item INT,
+	name_client VARCHAR(20),
+	yearr DATE,
+	city VARCHAR(30),
+	name_item VARCHAR(20)
 );
